608bd92a3d6d91f7a2995ba7661d59ba9a95337c
Denis P <denispoperechny@gmail.com>
2014-03-16 18:18:31 +0200

    Master page added

--- sources/django/map/urls.py
+++ sources/django/map/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from map import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^test/', views.test, name='test')
+    url(r'^$', views.index, name='index')
+    #,url(r'^test/', views.test, name='test')
 )

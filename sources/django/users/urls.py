69b9d8f9848aefdf082232454bb61767e40eda2a
Denis P <denispoperechny@gmail.com>
2014-04-20 17:04:37 +0300

    User management pages added

--- sources/django/users/urls.py
+++ sources/django/users/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
-
 from users import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^create/', views.create, name='create'),
+    url(r'^profile/', views.profile, name='profile')
     #,url(r'^test/', views.test, name='test')
 )
--- sources/django/users/views.py
+++ sources/django/users/views.py
@@ -3,10 +3,26 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 
-
-def index(request):
-    t = loader.get_template('users/index.html')
+def login(request):
+    t = loader.get_template('users/login.html')
     c = Context({
     #    'latest_poll_list': latest_poll_list,
     })
     return HttpResponse(t.render(c))
+
+def create(request):
+    t = loader.get_template('users/create.html')
+    c = Context({
+    #    'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(t.render(c))
+
+def profile(request):
+    t = loader.get_template('users/profile.html')
+    c = Context({
+    #    'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(t.render(c))
+
+def index(request):
+    return login(request)

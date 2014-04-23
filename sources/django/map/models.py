3c2525391edeeccd109af9e7d902a08857509f9e
Denis P <denispoperechny@gmail.com>
2014-04-24 00:14:02 +0300

    Model for Point entity added

--- sources/django/map/models.py
+++ sources/django/map/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Point(models.Model):
+    owner = models.ForeignKey(User)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=12, decimal_places=8)
+    longitude = models.DecimalField(max_digits=12, decimal_places=8)

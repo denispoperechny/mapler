187fc236c2f921ae8e21106516e3e183483906e9
Denis P <denispoperechny@gmail.com>
2014-04-27 14:25:52 +0300

    View switchers added

--- sources/django/map/views.py
+++ sources/django/map/views.py
@@ -1,22 +1,43 @@
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
 
-
-def overview(request):
+def getUserName(request):
 	userName = None
 	if request.user.is_authenticated():
 		userName = request.user.username
+	return userName
 
-	t = loader.get_template('map/index.html')
+def overview(request):
+	userName = getUserName(request)
+	if userName == None:
+		return HttpResponseRedirect("/")
+
+	t = loader.get_template('map/overwiev.html')
 	c = Context({
 		'userName': userName,
 	})
 	return HttpResponse(t.render(c))
 
 def managePoints(request):
-	return index(request)
+	userName = getUserName(request)
+	if userName == None:
+		return HttpResponseRedirect("/")
+
+	t = loader.get_template('map/manage-points.html')
+	c = Context({
+		'userName': userName,
+	})
+	return HttpResponse(t.render(c))
 
 def index(request):
-	return overview(request)
+	userName = getUserName(request)
+	if userName != None:
+		return HttpResponseRedirect("/map/overview")
+
+	t = loader.get_template('map/index.html')
+	c = Context({
+		# 'userName': userName,
+	})
+	return HttpResponse(t.render(c))
--- sources/django/users/views.py
+++ sources/django/users/views.py
@@ -1,7 +1,6 @@
 # Create your views here.
 
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
 
 from django.shortcuts import render

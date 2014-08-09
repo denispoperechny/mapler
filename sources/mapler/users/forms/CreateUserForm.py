77c1749d445e8a24a034a88a164e1a042a02e6f5
Denis <denispoperechny@gmail.com>
2014-08-10 00:39:16 +0300

    User agreement added

--- sources/mapler/users/forms/CreateUserForm.py
+++ sources/mapler/users/forms/CreateUserForm.py
@@ -4,3 +4,4 @@ class CreateUserForm(forms.Form):
     login = forms.CharField(max_length=20)
     password = forms.CharField(widget=forms.PasswordInput())
     mail = forms.EmailField(required=False)
+    agreement = forms.BooleanField(required = True)
